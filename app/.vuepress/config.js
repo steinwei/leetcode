1e43f253e47202c92579caf0aca30a63b153b1b7
steinwei <weishijiestein@outlook.com>
2020-06-29 09:23:18 +0800

    feat: init

--- app/.vuepress/config.js
+++ app/.vuepress/config.js
@@ -1,13 +1,37 @@
-console.log(`当前工作目录是: ${process.cwd()}`);
+// const fs = require('fs')
+// const path=require('path')
+// const filepath = path.join(__dirname,'../')
+// const regEx = /.vuepress/;
+// const regMd = /.README/
+// const files = fs.readdirSync(filepath).map(item=>{
+//     if(regEx.test(item)||regMd.test(item)){
+//         return null
+//     }
+//     return `/${item}/`
+// });
+const menuConfig = require("vuepress-bar")();
+
+const autobar_options = {
+  rootDir: 'xxx',
+  stripNumbers : true,
+  maxLevel : 2,
+  navPrefix : "nav",
+  skipEmptySidebar : true,
+  skipEmptyNavbar : true,
+  multipleSideBar : true,
+  setHomepage : 'top'
+};
 
 module.exports={
     base: '/leetcode/',
     dest: 'docs',
-    plugins: ['autobar'],
+    plugins: ['autobar', autobar_options],
     themeConfig: {
+      ...menuConfig,
         nav: [
             { text: 'Home', link: '/' }
           ],
+          // sidebar:files,
         displayAllHeaders: false // 默认值：false
       }
 }
