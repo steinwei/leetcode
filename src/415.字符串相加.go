057bbd6e66973e166126a3c8a4e63924c2fa7128
sjisntsuperman <weishijiestein@outlook.com>
2021-01-08 23:12:43 +0800

    feat: feat some solutions
    ]

---
+++ src/415.字符串相加.go
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=415 lang=golang
+ *
+ * [415] 字符串相加
+ */
+package leetcode
+
+import "strconv"
+
+// @lc code=start
+func addStrings(num1 string, num2 string) string {
+	ans := ""
+	add := 0
+	for i, j := len(num1)-1, len(num2)-1; i >= 0 || j >= 0 || add != 0; i, j = i-1, j-1 {
+		var x, y int
+		if i >= 0 {
+			x = int(num1[i] - '0')
+		}
+		if j >= 0 {
+			y = int(num2[j] - '0')
+		}
+		result := x + y + add
+		ans = strconv.Itoa(result%10) + ans
+		add = result / 10
+	}
+
+	return ans
+}
+
+// @lc code=end
