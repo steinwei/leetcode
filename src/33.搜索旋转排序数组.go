f50a8a46a511bd731b92cfc14531b599e06950b9
weishijie <weishijiestein@outlook.com>
2021-08-10 15:59:42 +0800

    feat: append solutions

---
+++ src/33.搜索旋转排序数组.go
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode.cn id=33 lang=golang
+ *
+ * [33] 搜索旋转排序数组
+ */
+
+package leetcode
+
+// @lc code=start
+func search(nums []int, target int) int {
+	n:=len(nums)
+
+	if n == 0 {
+		return -1
+	}
+
+	if n == 1 {
+		if nums[0] == target {
+			return 0
+		}
+		return -1
+	}
+
+	lo,hi:=0,n-1
+
+	for lo <= hi {
+		mid := (hi+lo)/2
+
+		// get result
+		if nums[mid] == target {
+			return mid
+		}
+		
+		// 判断单调区间
+		if nums[0] <= nums[mid] {
+			if nums[mid] > target && nums[0] <= target {
+				hi = mid - 1
+			} else {
+				lo = mid + 1
+			}
+		} else {
+			if nums[mid] < target && nums[n-1] >= target  {
+				lo = mid + 1
+			} else {
+				hi = mid - 1
+			}
+		}
+	}
+
+	return -1
+}
+// @lc code=end
+
